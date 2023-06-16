f276ec12e96728bfd352689c79e601a2d375f16b
tokyo4j <hrak1529@gmail.com>
2023-06-16 18:57:20 +0900

    a

--- update_limine.sh
+++ update_limine.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh -v
 
-cp /boot/*-ucode.img /boot/vmlinuz-linux /boot/initramfs-linux.img /efi/limine
-cp /usr/share/limine/BOOTX64.EFI /efi/EFI/BOOT/LIMINE.EFI
+cp -v /boot/*-ucode.img /boot/vmlinuz-linux /boot/initramfs-linux.img /efi/limine/linux
+cp -v /usr/share/limine/BOOTX64.EFI /efi/EFI/BOOT/LIMINE.EFI
 
 # To add boot entry:
 # efibootmgr -c -d /dev/sda -p 2 -L "Limine Bootloader" -l "\EFI\BOOT\LIMINE.EFI"
