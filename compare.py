327496ee5ba2194b1e2782158d093a221ffbc90e
tokyo4j <hrak1529@gmail.com>
2026-02-03 01:32:44 +0900

    compare.py: colored texts

--- compare.py
+++ compare.py
@@ -5,17 +5,26 @@ import filecmp
 import glob
 import re
 
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+RESET = '\033[0m'
+
+def print_colored(msg, color):
+    print(f"{color}{msg}{RESET}")
+
 for tmpl_file in glob.glob("config/**/*", recursive=True, include_hidden=True):
     if not os.path.isfile(tmpl_file):
         continue
     fs_file = re.sub("^config/", "/", tmpl_file)
     fs_file = re.sub("^/HOME/", f"{os.environ['HOME']}/", fs_file)
     if not os.path.exists(fs_file):
-        print("Not exist:", fs_file)
+        print_colored(f"Not exist: {fs_file}", RED)
         continue
     if filecmp.cmp(tmpl_file, fs_file):
-        print("Identical:", fs_file)
+        print_colored(f"Identical: {fs_file}", GREEN)
         continue
+    print_colored(f"Opening: {fs_file}", YELLOW)
     subprocess.call(
         ["code", "-d", fs_file, tmpl_file],
         stdout=subprocess.DEVNULL,
